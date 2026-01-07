3dc8d6d3d1dbe43c785d2317593338c5097905a0
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-01-07 21:46:45 +0400

    refactor(cli): adjust repository list keybindings
    Modify fzf keybindings for repository list to remove abort behavior and
    view readme option. This allows for more flexible interaction with
    repository commands.

--- scripts/gh_repo.sh
+++ scripts/gh_repo.sh
@@ -93,7 +93,6 @@ _gh_repo_list() {
 		--footer "$_fzf_icon GitHub Repositories" \
 		--bind "enter:execute(gh repo view {1})+abort" \
 		--bind "ctrl-o:execute-silent(gh repo view {1} --web)" \
-		--bind "alt-c:execute($_gh_repo_source_dir/gh_repo_cmd.sh clone {1})+abort" \
-		--bind "alt-f:execute($_gh_repo_source_dir/gh_repo_cmd.sh fork {1})+abort" \
-		--bind "alt-v:execute(gh repo view {1} --readme)+abort"
+		--bind "alt-c:execute($_gh_repo_source_dir/gh_repo_cmd.sh clone {1})" \
+		--bind "alt-f:execute($_gh_repo_source_dir/gh_repo_cmd.sh fork {1})"
 }
